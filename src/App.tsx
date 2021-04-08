9168fc789318093288e10ecf9a7fc6ab4966cb5d
Raghunathji <partabrajp@gmail.com>
2021-04-08 22:50:32 +0500

    2 steps complete - TS template and API / Json fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {getQuizDetails} from "./Services/quiz_services";
 
 function App() {
+
+useEffect(() => {
+  async function fetchData() {
+    const questions = await getQuizDetails(50, 'easy');
+    console.log(questions)
+  } 
+  fetchData();
+}, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>j sia raam</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
