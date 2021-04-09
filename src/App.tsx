dd87ca8212b9afc7985bdc969b37f31d53a5ef9d
Raghunathji <partabrajp@gmail.com>
2021-04-09 23:25:07 +0500

    quiz function working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {getQuizDetails} from "./Services/quiz_services";
+import { Quiz } from "./Types/quiz_types";
 
 function App() {
 
+let [quiz, setQuiz] = useState<Quiz[]>([])
+
 useEffect(() => {
   async function fetchData() {
-    const questions = await getQuizDetails(50, 'easy');
+    const questions = await getQuizDetails(5, 'easy');
     console.log(questions)
   } 
   fetchData();
--- src/Services/quiz_services.ts
+++ src/Services/quiz_services.ts
@@ -1,5 +1,15 @@
-export const getQuizDetails = async(totalQuestions: number, level:string)=>{
+import {Quiz, QuestionType  } from "../Types/quiz_types";
+
+export const getQuizDetails = async(totalQuestions: number, level:string): Promise<Quiz[]>=>{
     const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`);
-    let {results} = await res.json();
-    return results
+    // let data:QuestionType[] = await res.json();
+    let { results } = await res.json();
+    const quiz = results.map((questionObj:Quiz, ind: number)=>{
+        return {
+            question: questionObj.question,
+            answer: questionObj.correct_answer,
+            option: questionObj.incorrect_answers.concat(questionObj.correct_answer)
+        }
+    })
+    return quiz;
 }
