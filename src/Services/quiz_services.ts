916287b53f6910b41a3bd93dc99d0c7201acaa8f
Raghunathji <partabrajp@gmail.com>
2021-04-11 14:47:27 +0500

    REnder question is in process

--- src/Services/quiz_services.ts
+++ src/Services/quiz_services.ts
@@ -1,14 +1,17 @@
 import {Quiz, QuestionType  } from "../Types/quiz_types";
 
+const shuffleArray = (array: any[]) =>
+[...array].sort(()=>Math.random()-.5)
+
 export const getQuizDetails = async(totalQuestions: number, level:string): Promise<Quiz[]>=>{
     const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&difficulty=${level}&type=multiple`);
     // let data:QuestionType[] = await res.json();
     let { results } = await res.json();
-    const quiz = results.map((questionObj:Quiz, ind: number)=>{
+    const quiz:Quiz[] = results.map((questionObj:Quiz, ind: number)=>{
         return {
             question: questionObj.question,
             answer: questionObj.correct_answer,
-            option: questionObj.incorrect_answers.concat(questionObj.correct_answer)
+            option: shuffleArray(questionObj.incorrect_answers.concat(questionObj.correct_answer))
         }
     })
     return quiz;
